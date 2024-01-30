b4ab9626ec7410bc38d46c0be8325562360bb508
Gest prof <aim4ik001@gmail.com>
2024-01-30 12:53:33 +0000

    Add new file

---
+++ backend/api/webapp/schemas.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel
+from typing import Optional, List
+
+
+class NomenclatureCreate(BaseModel):
+    name: str
+    type: Optional[str]
+    description_short: Optional[str]
+    description_long: Optional[str]
+    code: Optional[int]
+    unit: Optional[int]
+    category: Optional[int]
+    manufacturer: Optional[int]
+
+    class Config:
+        orm_mode = True
+
+
+class NomenclatureGet(NomenclatureCreate):
+    id: int
+    unit_name: Optional[str]
+    updated_at: int
+    created_at: int
+
+    class Config:
+        orm_mode = True
+
+
+class NomenclatureListGetRes(BaseModel):
+    result: Optional[List[NomenclatureGet]]
+    count: int
