f3389820ed12ecf6bade01bd9973d713f159dea8
Gest prof <aim4ik001@gmail.com>
2024-01-31 07:06:52 +0000

    Update routers.py

--- backend/api/webapp/routers.py
+++ backend/api/webapp/routers.py
@@ -275,9 +275,9 @@ async def get_nomenclature(
 
         res.append(balance_dict)
 
-    for nomenclature in nomenclature_db:
-        print(nomenclature)
-        # query = pictures.select().where(pictures.c.entity_id == nomenclature.category.id)
-        # pictures_db = await database.fetch_all(query)
-        # nomenclature['pictures'] = pictures_db
+    # for nomenclature in nomenclature_db:
+    #     print(nomenclature)
+    #     query = pictures.select().where(pictures.c.entity_id == nomenclature.category.id)
+    #     pictures_db = await database.fetch_all(query)
+    #     nomenclature['pictures'] = pictures_db
     return {"result": nomenclature_db, "count": nomenclature_db_c.count_1}
