e3670e84707147bf4ded80d89ab16dfbf417de77
Gest23 <aim4ik001@gmail.com>
2024-02-07 00:04:07 +0300

    task1 test

--- backend/api/webapp/routers.py
+++ backend/api/webapp/routers.py
@@ -3,51 +3,21 @@ import api.webapp.schemas as schemas
 from database.db import database, pictures, price_types, warehouse_balances, prices
 from fastapi import APIRouter
 
-from typing import List, Optional
-from database.db import categories, database, manufacturers, nomenclature
-from fastapi import APIRouter, HTTPException
-from functions.helpers import (
-    check_entity_exists,
-    check_unit_exists,
-    datetime_to_timestamp,
-    get_entity_by_id,
-    get_user_by_token,
-    nomenclature_unit_id_to_name,
-)
-from sqlalchemy import func, select
-from ws_manager import manager
-
 router = APIRouter(tags=["webapp"])
 
 
-@router.get("/webapp/")
+@router.get("/webapp/", response_model=schemas.NomenclatureListGetRes)
 async def get_nomenclature(
         token: str,
         name: Optional[str] = None,
         limit: int = 100,
         offset: int = 0
 ):
-    """Получение фотографий, цен и их видов, остатков и названия категорий"""
+    """ Получение фотографий """
 
-    user = await get_user_by_token(token)
-
-    filters = [
-        nomenclature.c.cashbox == user.cashbox_id,
-        nomenclature.c.is_deleted.is_not(True),
-    ]
-
-    if name:
-        filters.append(nomenclature.c.name.ilike(f"%{name}%"))
-
-    query = nomenclature.select().where(*filters).limit(limit).offset(offset)
-
-    nomenclature_db = await database.fetch_all(query)
-    nomenclature_db = [*map(datetime_to_timestamp, nomenclature_db)]
-    nomenclature_db = [*map(nomenclature_unit_id_to_name, nomenclature_db)]
-    nomenclature_db = [await inst for inst in nomenclature_db]
-
-    query = select(func.count(nomenclature.c.id)).where(*filters)
-    nomenclature_db_c = await database.fetch_one(query)
+    from api.nomenclature.routers import get_nomenclature
+    response = await get_nomenclature(token, name, limit, offset)
+    nomenclature_db = response['result']
 
     for item in nomenclature_db:
         query = pictures.select().where(pictures.c.entity_id == item['id'])
