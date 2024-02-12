8c470ebc40804bc4e087c3d4973b36b81f379fdb
Gest23 <aim4ik001@gmail.com>
2024-02-13 02:09:37 +0300

    .

--- backend/api/webapp/routers.py
+++ backend/api/webapp/routers.py
@@ -98,7 +98,7 @@ async def get_nomenclature(
             filter_prices_price.append(prices.c.price_type >= filter_prices.date_from)
         if filter_prices.date_to:
             filter_prices_price.append(prices.c.date_to <= filter_prices.date_to)
-        q = (prices.select().where(prices.c.owner == user.id, prices.c.is_deleted is False, *filter_prices_price)
+        q = (prices.select().where(prices.c.owner == user.id, prices.c.is_deleted == False, *filter_prices_price)
                    .order_by(desc(prices.c.id))
              )
         prices_db = await database.fetch_all(q)
@@ -120,7 +120,7 @@ async def get_nomenclature(
 
                 nomenclature.c.id == price_db.nomenclature,
                 nomenclature.c.owner == user.id,
-                nomenclature.c.is_deleted is False,
+                nomenclature.c.is_deleted == False,
                 *filter_prices_nom,
             )
             nom_db = await database.fetch_one(q)
