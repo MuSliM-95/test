0c037564427fe2b216803ce9871cd9d7b53380ae
Gest23 <aim4ik001@gmail.com>
2024-02-13 03:47:54 +0300

    .

--- backend/api/webapp/routers.py
+++ backend/api/webapp/routers.py
@@ -16,7 +16,7 @@ import api.webapp.schemas as schemas
 router = APIRouter(tags=["webapp"])
 
 
-@router.get("/webapp/")
+@router.get("/webapp/", response_model=schemas.WebappResponse)
 async def get_nomenclature(
         token: str,
         warehouse_id: Optional[int] = None,
@@ -70,8 +70,7 @@ async def get_nomenclature(
         query = prices.select().where(prices.c.nomenclature == item['id'])
         price_db = await database.fetch_one(query)
         # print(price_db.price_type)  ошибку дропает то есть ошибка в том что выдает None
-        #  price db не выдает нон
-        return price_db.price_type
+        #  price db не выдает нон и нормально выдает значение
         query = price_types.select().where(price_types.c.id == price_db.price_type,
                                            price_types.c.owner == user.id,
                                            price_types.c.is_deleted.is_not(True))
