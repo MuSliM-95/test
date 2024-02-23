80e92684ada392169c8a26151b8fdc793f06f200
alex zhiv <milanzerg@gmail.com>
2024-02-23 13:56:19 +0000

    Update file jobs.py

--- backend/jobs.py
+++ backend/jobs.py
@@ -7,7 +7,7 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 from pytz import utc
 from const import PAID, DEMO, RepeatPeriod
-from database.db import engine, accounts_balances, database, tariffs, payments
+from database.db import engine, accounts_balances, database, tariffs, payments, loyality_transactions, loyality_cards
 from functions.account import make_account
 from functions.goods_distribution import process_distribution
 from functions.gross_profit import process_gross_profit_report
@@ -49,6 +49,26 @@ async def check_account():
                 await make_account(balance)
         elif balance.tariff_type == PAID:
             await make_account(balance)
+
+
+@scheduler.scheduled_job("interval", seconds=5)
+async def autoburn():
+    await database.connect()
+
+    q = loyality_transactions.select()
+    all_transactions = await database.fetch_all(q)
+
+    for transaction in all_transactions:
+        if transaction.type == "autoburned":
+            loyality_card_id = transaction.loyality_card_id
+            loyality_transactions_id = transaction.id
+            created_at = int(transaction.created_at.timestamp())
+            current_time = int(datetime.utcnow().timestamp())
+            card = await database.fetch_one(loyality_cards.select().where(loyality_cards.id == loyality_card_id))
+            lifetime = int(card.lifetime)
+            if current_time >= created_at + lifetime:
+                query = loyality_transactions.delete().where(loyality_transactions.id == loyality_transactions_id)
+                await database.execute(query)
 
 
 # @scheduler.scheduled_job("interval", seconds=amo_interval)
