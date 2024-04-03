e138b08fb3efbebfa6c828c4539bf6d126d60df5
andreytretiachenko <andrey.tretiachenko@gmail.com>
2024-04-03 23:28:07 +0300

    fix reports

--- backend/api/reports/routers.py
+++ backend/api/reports/routers.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from sqlalchemy import select, func, desc, case, and_, text
-from database.db import database, payments, pboxes
+from database.db import database, payments, pboxes, docs_sales, docs_sales_goods, nomenclature
 from . import schemas
 from functions.helpers import get_user_by_token
 
@@ -8,10 +8,25 @@ from functions.helpers import get_user_by_token
 router = APIRouter(tags=["reports"])
 
 
-@router.get("/reports/sales/")
+@router.post("/reports/sales/")
 async def get_sales_report(token: str, report_data: schemas.ReportData):
     user = await get_user_by_token(token)
-    pass
+
+    sales = (
+            select(nomenclature.c.id,
+                   payments.c.paybox,
+                   nomenclature.c.name,
+                   func.sum(docs_sales_goods.c.quantity).label('count'),
+                   func.sum(docs_sales_goods.c.quantity)*docs_sales_goods.c.price
+                   ).
+            where(payments.c.paybox == 1).
+            join(payments, payments.c.docs_sales_id == docs_sales_goods.c.docs_sales_id).
+            join(nomenclature, nomenclature.c.id == docs_sales_goods.c.nomenclature).
+            join(docs_sales, docs_sales.c.id == docs_sales_goods.c.docs_sales_id).
+            group_by(nomenclature.c.id,payments.c.paybox,nomenclature.c.name)
+             )
+    print(sales)
+    return {}
 
 
 @router.post("/reports/balances/")
