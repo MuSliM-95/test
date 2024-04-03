9254f06589ec194d53e5b74d5456b69a4ee87c5e
Директор IT <meb290@mail.ru>
2024-04-03 19:25:38 +0300

    fix reports

--- backend/api/reports/routers.py
+++ backend/api/reports/routers.py
@@ -53,6 +53,8 @@ async def get_balances_report(token: str, report_data: schemas.ReportData):
                   ).\
             join(pboxes, pboxes.c.id == payments.c.paybox).\
             group_by(payments.c.paybox, payments.c.type, pboxes.c.name).subquery('query_outgoing')
+        report_db_in = await database.fetch_all(query_incoming)
+        print([dict(item) for item in report_db_in])
 
         query = select(pboxes.c.name, query_incoming.c.incoming, query_outgoing.c.outgoing).where(pboxes.c.id == paybox)
 
