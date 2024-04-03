845b11358354170f5ccbdb74b43df66cad2d688c
Директор IT <meb290@mail.ru>
2024-04-03 10:49:56 +0300

    fix reports

--- backend/api/reports/routers.py
+++ backend/api/reports/routers.py
@@ -17,37 +17,39 @@ async def get_sales_report(token: str):
 @router.post("/reports/balances/")
 async def get_balances_report(token: str, report_data: schemas.ReportData):
     await get_user_by_token(token)
+    report = []
+    for paybox in report_data.paybox:
+        filters = [
+            payments.c.paybox == paybox,
+            and_(report_data.datefrom <= payments.c.date, payments.c.date <= report_data.dateto),
+            payments.c.is_deleted.is_not(True)
+        ]
 
-    filters = [
-        payments.c.paybox.in_(report_data.paybox),
-        and_(report_data.datefrom <= payments.c.date, payments.c.date <= report_data.dateto),
-        payments.c.is_deleted.is_not(True)
-    ]
+        if report_data.user:
+            filters.append(payments.c.account == report_data.user)
 
-    if report_data.user:
-        filters.append(payments.c.account == report_data.user)
+        query_incoming = select(
+            payments.c.paybox,
+            pboxes.c.name,
+            payments.c.type,
+            func.sum(payments.c.amount).label('incoming'),
+        ).\
+            where(*filters, payments.c.type == 'incoming').\
+            join(pboxes, pboxes.c.id == payments.c.paybox).\
+            group_by(payments.c.paybox, payments.c.type, pboxes.c.name).subquery('query_incoming')
 
-    query_incoming = select(
-        payments.c.paybox,
-        pboxes.c.name,
-        payments.c.type,
-        func.sum(payments.c.amount).label('incoming'),
-    ).\
-        where(*filters, payments.c.type == 'incoming').\
-        join(pboxes, pboxes.c.id == payments.c.paybox).\
-        group_by(payments.c.paybox, payments.c.type, pboxes.c.name).subquery('query_incoming')
+        query_outgoing = select(
+            payments.c.paybox,
+            pboxes.c.name,
+            payments.c.type,
+            func.sum(payments.c.amount).label('outgoing'),
+        ).\
+            where(*filters, payments.c.type == 'outgoing').\
+            join(pboxes, pboxes.c.id == payments.c.paybox).\
+            group_by(payments.c.paybox, payments.c.type, pboxes.c.name).subquery('query_outgoing')
 
-    query_outgoing = select(
-        payments.c.paybox,
-        pboxes.c.name,
-        payments.c.type,
-        func.sum(payments.c.amount).label('outgoing'),
-    ).\
-        where(*filters, payments.c.type == 'outgoing').\
-        join(pboxes, pboxes.c.id == payments.c.paybox).\
-        group_by(payments.c.paybox, payments.c.type, pboxes.c.name).subquery('query_outgoing')
+        query = select(pboxes.c.name, query_incoming.c.incoming, query_outgoing.c.outgoing).where(pboxes.c.id == paybox)
 
-    query = select(pboxes.c.name, query_incoming.c.incoming, query_outgoing.c.outgoing).where(pboxes.c.id.in_(report_data.paybox))
+        report.append(await database.fetch_one(query))
 
-    report = await database.fetch_all(query)
     return report
