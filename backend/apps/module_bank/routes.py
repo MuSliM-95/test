812c78af14930f1f0cf0ab927db69de84ecbb840
Evtushevskiy Maksim <gfer-6@mail.ru>
2024-08-06 16:55:17 +0600

    * Add logs for auth module bank

--- backend/apps/module_bank/routes.py
+++ backend/apps/module_bank/routes.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 import aiohttp
 from fastapi import APIRouter, HTTPException
 from sqlalchemy import and_
@@ -18,7 +16,7 @@ async def moduloauth(code: str, state: int):
     """Hook для oauth банка"""
 
     user_integration = await integration_info(state, 3)
-
+    print(user_integration)
     if not user_integration:
         raise HTTPException(status_code=432, detail=f"user not found with integration")
 
@@ -30,9 +28,12 @@ async def moduloauth(code: str, state: int):
                     'clientId': user_integration.get('client_app_id'),
                     'clientSecret': user_integration.get('client_secret'),
                 },
-                headers={'Content-type': 'application/json'}
+                headers={
+                    'Content-type': 'application/json'
+                }
         ) as resp:
             token_json = await resp.json()
+            print(token_json)
     if token_json.get('error'):
         raise HTTPException(status_code=432, detail=f"error OAuth2 {token_json.get('error')}")
     try:
@@ -50,75 +51,75 @@ async def moduloauth(code: str, state: int):
     except Exception as error:
         raise HTTPException(status_code=433, detail=str(error))
 
-    async with aiohttp.ClientSession(trust_env=True) as session:
-        async with session.get(f'https://api.modulbank.ru/v1/account-info',
-                               headers={
-                                   'Authorization': f'Bearer {token_json.get("access_token")}',
-                                   'Content-type': 'application/json'
-                               }) as resp:
-            companies_json = await resp.json()
-    for company in companies_json:
-        for bank_account in company.get("bankAccounts"):
-            data = {
-                'name': f"Счет банк Модуль №{bank_account.get('id')}",
-                'start_balance': 0,
-                'cashbox': state,
-                'balance': bank_account.get("balance"),
-                'update_start_balance': int(datetime.utcnow().timestamp()),
-                'update_start_balance_date': int(datetime.utcnow().timestamp()),
-                'created_at': int(datetime.utcnow().timestamp()),
-                'updated_at': int(datetime.utcnow().timestamp()),
-                'balance_date': 0
-            }
-            account_db = await database.fetch_one(
-                module_bank_accounts.select().where(module_bank_accounts.c.accountId == bank_account.get('id')))
-            if not account_db:
-                id_paybox = await database.execute(pboxes.insert().values(data))
-                await database.execute(module_bank_accounts.insert().values(
-                    {
-                        'payboxes_id': id_paybox,
-                        'module_bank_credential_id': credentials_id,
-                        'accountName': bank_account.get('accountName'),
-                        'bankBic': bank_account.get('bankBic'),
-                        'bankInn': bank_account.get('bankInn'),
-                        'bankKpp': bank_account.get('bankKpp'),
-                        'bankCorrespondentAccount': bank_account.get('bankCorrespondentAccount'),
-                        'bankName': bank_account.get('bankName'),
-                        'beginDate': bank_account.get('beginDate'),
-                        'category': bank_account.get('category'),
-                        'currency': bank_account.get('currency'),
-                        'accountId': bank_account.get('id'),
-                        'number': bank_account.get('number'),
-                        'status': bank_account.get('status'),
-                        'is_deleted': False,
-                        'is_active': False
-                    }
-                ))
-            else:
-                del data['created_at']
-                id_paybox = await database.execute(
-                    pboxes.update().where(pboxes.c.id == account_db.get('payboxes_id')).values(data))
-                await database.execute(
-                    module_bank_accounts.update().where(module_bank_accounts.c.id == account_db.get('id')).values(
-                        {
-                            'payboxes_id': id_paybox,
-                            'module_bank_credential_id': credentials_id,
-                            'accountName': bank_account.get('accountName'),
-                            'bankBic': bank_account.get('bankBic'),
-                            'bankInn': bank_account.get('bankInn'),
-                            'bankKpp': bank_account.get('bankKpp'),
-                            'bankCorrespondentAccount': bank_account.get('bankCorrespondentAccount'),
-                            'bankName': bank_account.get('bankName'),
-                            'beginDate': bank_account.get('beginDate'),
-                            'category': bank_account.get('category'),
-                            'currency': bank_account.get('currency'),
-                            'accountId': bank_account.get('id'),
-                            'number': bank_account.get('number'),
-                            'status': bank_account.get('status'),
-                            'is_deleted': False,
-                            'is_active': False
-                        }
-                    ))
+    # async with aiohttp.ClientSession(trust_env=True) as session:
+    #     async with session.get(f'https://api.modulbank.ru/v1/account-info',
+    #                            headers={
+    #                                'Authorization': f'Bearer {token_json.get("access_token")}',
+    #                                'Content-type': 'application/json'
+    #                            }) as resp:
+    #         companies_json = await resp.json()
+    # for company in companies_json:
+    #     for bank_account in company.get("bankAccounts"):
+    #         data = {
+    #             'name': f"Счет банк Модуль №{bank_account.get('id')}",
+    #             'start_balance': 0,
+    #             'cashbox': state,
+    #             'balance': bank_account.get("balance"),
+    #             'update_start_balance': int(datetime.utcnow().timestamp()),
+    #             'update_start_balance_date': int(datetime.utcnow().timestamp()),
+    #             'created_at': int(datetime.utcnow().timestamp()),
+    #             'updated_at': int(datetime.utcnow().timestamp()),
+    #             'balance_date': 0
+    #         }
+    #         account_db = await database.fetch_one(
+    #             module_bank_accounts.select().where(module_bank_accounts.c.accountId == bank_account.get('id')))
+    #         if not account_db:
+    #             id_paybox = await database.execute(pboxes.insert().values(data))
+    #             await database.execute(module_bank_accounts.insert().values(
+    #                 {
+    #                     'payboxes_id': id_paybox,
+    #                     'module_bank_credential_id': credentials_id,
+    #                     'accountName': bank_account.get('accountName'),
+    #                     'bankBic': bank_account.get('bankBic'),
+    #                     'bankInn': bank_account.get('bankInn'),
+    #                     'bankKpp': bank_account.get('bankKpp'),
+    #                     'bankCorrespondentAccount': bank_account.get('bankCorrespondentAccount'),
+    #                     'bankName': bank_account.get('bankName'),
+    #                     'beginDate': bank_account.get('beginDate'),
+    #                     'category': bank_account.get('category'),
+    #                     'currency': bank_account.get('currency'),
+    #                     'accountId': bank_account.get('id'),
+    #                     'number': bank_account.get('number'),
+    #                     'status': bank_account.get('status'),
+    #                     'is_deleted': False,
+    #                     'is_active': False
+    #                 }
+    #             ))
+    #         else:
+    #             del data['created_at']
+    #             id_paybox = await database.execute(
+    #                 pboxes.update().where(pboxes.c.id == account_db.get('payboxes_id')).values(data))
+    #             await database.execute(
+    #                 module_bank_accounts.update().where(module_bank_accounts.c.id == account_db.get('id')).values(
+    #                     {
+    #                         'payboxes_id': id_paybox,
+    #                         'module_bank_credential_id': credentials_id,
+    #                         'accountName': bank_account.get('accountName'),
+    #                         'bankBic': bank_account.get('bankBic'),
+    #                         'bankInn': bank_account.get('bankInn'),
+    #                         'bankKpp': bank_account.get('bankKpp'),
+    #                         'bankCorrespondentAccount': bank_account.get('bankCorrespondentAccount'),
+    #                         'bankName': bank_account.get('bankName'),
+    #                         'beginDate': bank_account.get('beginDate'),
+    #                         'category': bank_account.get('category'),
+    #                         'currency': bank_account.get('currency'),
+    #                         'accountId': bank_account.get('id'),
+    #                         'number': bank_account.get('number'),
+    #                         'status': bank_account.get('status'),
+    #                         'is_deleted': False,
+    #                         'is_active': False
+    #                     }
+    #                 ))
 
     return RedirectResponse(f'https://app.tablecrm.com/integrations?token={user_integration.get("token")}')
 
