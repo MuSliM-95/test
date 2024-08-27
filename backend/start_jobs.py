f197d0b4c7f2721f5caf7b0be99989fb51a6636e
Максим <gfer-6@mail.ru>
2024-08-28 00:53:04 +0600

    Fix jobs

--- backend/start_jobs.py
+++ backend/start_jobs.py
@@ -5,13 +5,8 @@ from jobs import scheduler
 IS_RUN_STATE = True
 
 def my_any_func():
-    IS_RUN_STATE = False
+    scheduler.shutdown()
 
 if __name__ == "__main__":
     atexit.register(my_any_func)
-    try:
-        scheduler.start()
-        while IS_RUN_STATE:
-            pass
-    finally:
-        scheduler.shutdown()
+    scheduler.start()
