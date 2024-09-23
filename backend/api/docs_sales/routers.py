73d38d3d860c332aa89d660d74b6f88a53f80994
motyuka <sa2508mc@gmail.com>
2024-09-23 21:17:57 +0300

    fix

--- backend/api/docs_sales/routers.py
+++ backend/api/docs_sales/routers.py
@@ -319,8 +319,8 @@ async def create(token: str, docs_sales_data: schemas.CreateMass, generate_out: 
 
         goods: Union[list, None] = instance_values.pop("goods", None)
 
-        paid_rubles = instance_values.pop("paid_rubles")
-        paid_lt = instance_values.pop("paid_lt")
+        paid_rubles = instance_values.pop("paid_rubles", 0)
+        paid_lt = instance_values.pop("paid_lt", 0)
         lt = instance_values.pop("loyality_card_id")
 
         if not await check_period_blocked(
