05408a0c502fd6afa0aa810929fe03c474c07284
Максим <gfer-6@mail.ru>
2024-10-10 23:34:59 +0600

    Fix Patch Client Token

--- backend/apps/amocrm/api/installer/infrastructure/models/PatchWidgetInstallerInfoModel.py
+++ backend/apps/amocrm/api/installer/infrastructure/models/PatchWidgetInstallerInfoModel.py
@@ -5,7 +5,7 @@ from pydantic import BaseModel
 
 class PatchWidgetInstallerInfoModel(BaseModel):
     client_name: Optional[str]
-    client_cashbox: Optional[str]
+    client_cashbox: Optional[int]
     client_number_phone: Optional[str]
 
     partner_name: Optional[str]
