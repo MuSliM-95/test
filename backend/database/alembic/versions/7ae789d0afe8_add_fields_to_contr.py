1e3a9da20bc3d2949ad5a7c47068c17f07943f5c
motyuka <sa2508mc@gmail.com>
2024-11-09 23:23:42 +0300

    add fields to contragents

---
+++ backend/database/alembic/versions/7ae789d0afe8_add_fields_to_contr.py
@@ -0,0 +1,24 @@
+"""add fields to contr
+
+Revision ID: 7ae789d0afe8
+Revises: fab805a9d974
+Create Date: 2024-11-09 23:23:29.060643
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '7ae789d0afe8'
+down_revision = 'fab805a9d974'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
