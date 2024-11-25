f53e17d3a55615c20b9c68ce4d8adde634f0c7f7
motyuka <sa2508mc@gmail.com>
2024-11-25 16:24:10 +0300

    add check_account to new jobs view
    (cherry picked from commit 9a83e2ec64f0d0efefc3322bb79a38d3086a8789)

---
+++ backend/jobs/check_account/job.py
@@ -0,0 +1,20 @@
+from datetime import datetime, timedelta
+
+from const import PAID, DEMO
+from database.db import accounts_balances, database, tariffs
+from functions.account import make_account
+
+
+async def check_account():
+    await database.connect()
+    balances = await database.fetch_all(accounts_balances.select())
+    tariff = await database.fetch_one(tariffs.select().where(tariffs.c.actual == True))
+    for balance in balances:
+        if balance.tariff_type == DEMO:
+            now = datetime.utcnow()
+            if now >= datetime.fromtimestamp(balance.created_at) + timedelta(
+                    days=tariff.demo_days
+            ):
+                await make_account(balance)
+        elif balance.tariff_type == PAID:
+            await make_account(balance)
