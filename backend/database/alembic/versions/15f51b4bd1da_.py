8ff856a0981bcf118ecc36bfb22b133681f80939
Максим Евтушевский <gfer-6@mail.ru>
2024-11-27 11:23:34 +0000

    Update 15f51b4bd1da_.py

--- backend/database/alembic/versions/15f51b4bd1da_.py
+++ backend/database/alembic/versions/15f51b4bd1da_.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '15f51b4bd1da'
-down_revision = None
+down_revision = '8eb1872d5402'
 branch_labels = None
 depends_on = None
 
