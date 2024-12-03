843528c4acb89b6414b34ba81a60599ae066dba5
Директор IT <meb290@mail.ru>
2024-12-03 18:48:47 +0300

    update triggers: post, patch, delete

--- backend/api/settings/amo_triggers/schemas.py
+++ backend/api/settings/amo_triggers/schemas.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from datetime import datetime
 from typing import Optional, List
 
 from database.enums import TriggerType, TriggerTime
@@ -8,6 +9,23 @@ class Filtersamobot(BaseModel):
     name: str = None
 
 
+class AmoBotView(BaseModel):
+    id: int
+    name: str
+
+
+class ViewTrigger(BaseModel):
+    id: int
+    name: str
+    amo_bot: AmoBotView
+    type: TriggerType
+    time_variant: TriggerTime
+    time: int
+    active: bool = False
+    created_at: datetime
+    updated_at: datetime
+
+
 class CreateTrigger(BaseModel):
     name: str
     amo_bots_id: int
@@ -15,3 +33,12 @@ class CreateTrigger(BaseModel):
     time_variant: TriggerTime
     time: int
     active: bool = False
+
+
+class PatchTrigger(BaseModel):
+    name: str = None
+    amo_bots_id: int = None
+    type: TriggerType = None
+    time_variant: TriggerTime = None
+    time: int = None
+    active: bool = None
