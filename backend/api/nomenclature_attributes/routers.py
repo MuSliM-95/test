ac1753d3ad240af594bb6fdbf4693ffd45274331
brokewe <hqwerov@yandex.ru>
2024-12-23 20:40:21 +0300

    routers/ new_nomenclature_attributes write cashbox_id,new_nomenclature_attribute_value checking the value in one request,create_nomenclature_group insert in one request,

--- backend/api/nomenclature_attributes/routers.py
+++ backend/api/nomenclature_attributes/routers.py
@@ -1,9 +1,8 @@
-from sqlite3 import IntegrityError
 from typing import List
-
+from sqlalchemy.exc import IntegrityError
 import api.nomenclature_attributes.schemas as schemas
 from database.db import categories, database, manufacturers, nomenclature, nomenclature_barcodes, prices, price_types, \
-    warehouse_register_movement, warehouses, units, warehouse_balances, nomenclature_attributes, nomenclature_attributes_value, nomenclature_groups
+    warehouse_register_movement, warehouses, units, warehouse_balances, nomenclature_attributes, nomenclature_attributes_value, nomenclature_groups, nomenclature_relations
 from fastapi import APIRouter, HTTPException
 
 from functions.helpers import (
@@ -30,7 +29,9 @@ async def new_nomenclature_attributes(
     :param token: Токен для проверки пользователя.
     :param attribute_data: Данные для создания атрибута.
     :return: Созданный атрибут.
+
     """
+
     # Проверяем токен пользователя
     user = await get_user_by_token(token)
     if not user:
@@ -46,14 +47,15 @@ async def new_nomenclature_attributes(
     # Создаем новый атрибут
     query = nomenclature_attributes.insert().values(
         name=attribute_data.name,
-        alias=attribute_data.alias
+        alias=attribute_data.alias,
+        cash_box=user.cashbox_id,
     )
     new_attribute_id = await database.execute(query)
 
     return schemas.AttributeCreateResponse(id=new_attribute_id, name=attribute_data.name, alias=attribute_data.alias)
 
 
-@router.post("/nomenclature/attributes-value", response_model=schemas.AttributeValueResponse)
+@router.post("/nomenclature/attributes_value", response_model=schemas.AttributeValueResponse)
 async def new_nomenclature_attribute_value(
     token: str,
     attribute_value_data: schemas.AttributeValueCreate
@@ -64,47 +66,64 @@ async def new_nomenclature_attribute_value(
     :param token: Токен пользователя.
     :param attribute_value_data: Данные для создания значения атрибутов.
     :return: Подтверждение сохранения.
+
     """
+
     # Проверка токена пользователя
     user = await get_user_by_token(token)
     if not user:
         raise HTTPException(status_code=401, detail="Недействительный токен")
 
     # Проверка существования номенклатуры
-    query = select(nomenclature.c.id).where(nomenclature.c.id == attribute_value_data.nomenclature_id)
+    query = select(nomenclature.c.id).where(and_(
+        nomenclature.c.id == attribute_value_data.nomenclature_id,
+        nomenclature.c.cashbox == user.cashbox_id,
+    ))
+
     nomenclature_record = await database.fetch_one(query)
     if not nomenclature_record:
         raise HTTPException(
             status_code=404, detail=f"Номенклатура с ID '{attribute_value_data.nomenclature_id}' не найдена"
         )
 
-    # Проверка существования атрибутов и сбор значений для вставки
+    # Проверка существования атрибутов одним запросом
+    attribute_ids = [attribute.attribute_id for attribute in attribute_value_data.attributes]
+    query = select(nomenclature_attributes.c.id).where(nomenclature_attributes.c.id.in_(attribute_ids))
+    existing_attribute_ids = {record["id"] for record in await database.fetch_all(query)}
+
+    # Получение уже существующих значений для номенклатуры и атрибутов
+    query = select(nomenclature_attributes_value.c.attribute_id, nomenclature_attributes_value.c.value).where(
+        nomenclature_attributes_value.c.nomenclature_id == attribute_value_data.nomenclature_id
+    )
+    existing_values = await database.fetch_all(query)
+
+    # Сопоставление существующих значений с их атрибутами
+    existing_values_map = {}
+    for record in existing_values:
+        if record["attribute_id"] not in existing_values_map:
+            existing_values_map[record["attribute_id"]] = set()
+        existing_values_map[record["attribute_id"]].add(record["value"])
+
+    # Формирование данных для вставки
     attributes_to_insert = []
     for attribute in attribute_value_data.attributes:
-        # Проверка существования атрибута
-        query = select(nomenclature_attributes.c.id).where(nomenclature_attributes.c.id == attribute.attribute_id)
-        attribute_record = await database.fetch_one(query)
-        if not attribute_record:
+        if attribute.attribute_id not in existing_attribute_ids:
             raise HTTPException(
                 status_code=404, detail=f"Атрибут с ID '{attribute.attribute_id}' не найден"
             )
-
-        # Обработка значения: список или строка
-        if isinstance(attribute.value, list):
-            for single_value in attribute.value:
-                attributes_to_insert.append({
-                    "nomenclature_id": attribute_value_data.nomenclature_id,
-                    "attribute_id": attribute.attribute_id,
-                    "value": single_value
-                })
-        else:
+        for single_value in attribute.value:
+            if single_value in existing_values_map.get(attribute.attribute_id, set()):
+                raise HTTPException(
+                    status_code=400,
+                    detail=f"Значение '{single_value}' для атрибута с ID '{attribute.attribute_id}' уже существует."
+                )
             attributes_to_insert.append({
                 "nomenclature_id": attribute_value_data.nomenclature_id,
                 "attribute_id": attribute.attribute_id,
-                "value": attribute.value
+                "value": single_value
             })
 
-    # Вставка значений атрибутов в базу данных
+    # Вставка данных в таблицу
     try:
         query = nomenclature_attributes_value.insert()
         await database.execute_many(query, attributes_to_insert)
@@ -113,15 +132,23 @@ async def new_nomenclature_attribute_value(
             status_code=400,
             detail="Ошибка: Уже существует значение для этого атрибута и номенклатуры."
         )
-    return schemas.AttributeValueResponse(nomenclature_id=attribute_value_data.nomenclature_id, attributes=attributes_to_insert)
+
+    return schemas.AttributeValueResponse(
+        nomenclature_id=attribute_value_data.nomenclature_id,
+        attributes=attribute_value_data.attributes
+    )
+
 
 @router.get("/nomenclature/{nomenclature_id}/attributes", response_model=List[schemas.NomenclatureAttribute])
 async def get_nomenclature_attributes(nomenclature_id: int, token: str):
     """
     Возвращает список атрибутов и их значений для заданной номенклатуры.
+
     :param nomenclature_id: ID номенклатуры.
     :param token: Токен пользователя.
+
     """
+
     # Проверка токена пользователя
     user = await get_user_by_token(token)
     if not user:
@@ -140,10 +167,9 @@ async def get_nomenclature_attributes(nomenclature_id: int, token: str):
             )
         )
         .where(nomenclature_attributes_value.c.nomenclature_id == nomenclature_id)
-        .order_by(nomenclature_attributes.c.name)  # Упорядочиваем по имени атрибута
+        .order_by(nomenclature_attributes.c.name)
     )
 
-    # Выполнение запроса
     results = await database.fetch_all(query)
 
     if not results:
@@ -158,13 +184,16 @@ async def get_nomenclature_attributes(nomenclature_id: int, token: str):
     return attributes
 
 
-@router.post("/nomenclature/make-groups", response_model=dict)
+@router.post("/nomenclature/make_groups", response_model=dict)
 async def create_nomenclature_group(token: str, data: schemas.NomenclatureRelations):
     """
     Объединяет номенклатуры в группу.
+
     :param token: Токен пользователя.
     :param data: Список ID номенклатур для объединения в группу.
+
     """
+
     # Проверка токена пользователя
     user = await get_user_by_token(token)
     if not user:
@@ -189,21 +218,27 @@ async def create_nomenclature_group(token: str, data: schemas.NomenclatureRelati
             detail="Один или несколько указанных ID номенклатур не существуют."
     )
 
-    # Генерация новой group_id
-    group_id_result = await database.fetch_one(
-        select(func.max(nomenclature_groups.c.group_id).label("max_group_id"))
+    # Создаем новую группу
+    new_group_id = await database.execute(
+        nomenclature_groups.insert().values()
     )
-    new_group_id = (group_id_result["max_group_id"] or 0) + 1  # Следующее значение group_id
 
-    # Вставка записей для каждой номенклатуры в группу
+    # Формируем данные для вставки
+    insert_data = [
+        {
+            "nomenclature_id": nomenclature_id,
+            "group_id": new_group_id,
+        }
+        for nomenclature_id in data.nomenclature_ids
+    ]
+
+    # Вставка всех записей в таблицу nomenclature_relations
     try:
         async with database.transaction():
-            for nomenclature_id in data.nomenclature_ids:
-                query = insert(nomenclature_groups).values(
-                    nomenclature_id=nomenclature_id,
-                    group_id=new_group_id
-                )
-                await database.execute(query)
+            await database.execute_many(
+                nomenclature_relations.insert(),
+                insert_data
+            )
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Не удалось создать группу: {str(e)}")
 
@@ -213,17 +248,20 @@ async def create_nomenclature_group(token: str, data: schemas.NomenclatureRelati
 async def get_group_for_nomenclature(token: str, nomenclature_id: int):
     """
     Получает group_id для указанной номенклатуры.
+
     :param token: Токен пользователя.
     :param nomenclature_id: ID номенклатуры.
+
     """
+
     # Проверка токена пользователя
     user = await get_user_by_token(token)
     if not user:
         raise HTTPException(status_code=401, detail="Недействительный токен")
 
     # Запрос для получения group_id
-    query = select(nomenclature_groups.c.group_id).where(
-        nomenclature_groups.c.nomenclature_id == nomenclature_id
+    query = select(nomenclature_relations.c.group_id).where(
+        nomenclature_relations.c.nomenclature_id == nomenclature_id
     )
     result = await database.fetch_one(query)
 
@@ -231,7 +269,7 @@ async def get_group_for_nomenclature(token: str, nomenclature_id: int):
         raise HTTPException(
             status_code=404,
             detail=f"Группа не найдена для номенклатуры с ID {nomenclature_id}"
-    )
+        )
 
     return schemas.NomenclatureGroupResponse(
         nomenclature_id=nomenclature_id,
@@ -242,10 +280,13 @@ async def get_group_for_nomenclature(token: str, nomenclature_id: int):
 async def add_nomenclature_to_group(token: str, group_id: int, nomenclature_id: int):
     """
     Добавляет номенклатуру в существующую группу.
+
     :param token: Токен пользователя.
     :param group_id: ID группы, в которую нужно добавить номенклатуру.
     :param nomenclature_id: ID номенклатуры, которую нужно добавить.
+
     """
+
     # Проверка токена пользователя
     user = await get_user_by_token(token)
     if not user:
@@ -253,7 +294,7 @@ async def add_nomenclature_to_group(token: str, group_id: int, nomenclature_id: 
 
     # Проверка существования group_id
     group_exists = await database.fetch_one(
-        select(nomenclature_groups.c.group_id).where(nomenclature_groups.c.group_id == group_id)
+        select(nomenclature_groups.c.id).where(nomenclature_groups.c.id == group_id)
     )
     if not group_exists:
         raise HTTPException(status_code=404, detail=f"Группа с ID {group_id} не существует")
@@ -269,9 +310,9 @@ async def add_nomenclature_to_group(token: str, group_id: int, nomenclature_id: 
 
     # Проверка, что номенклатура ещё не в группе
     already_in_group = await database.fetch_one(
-        select(nomenclature_groups.c.id).where(
-            (nomenclature_groups.c.group_id == group_id) &
-            (nomenclature_groups.c.nomenclature_id == nomenclature_id)
+        select(nomenclature_relations.c.id).where(
+            (nomenclature_relations.c.group_id == group_id) &
+            (nomenclature_relations.c.nomenclature_id == nomenclature_id)
         )
     )
     if already_in_group:
@@ -282,7 +323,7 @@ async def add_nomenclature_to_group(token: str, group_id: int, nomenclature_id: 
 
     # Добавляем номенклатуру в группу
     try:
-        query = insert(nomenclature_groups).values(
+        query = insert(nomenclature_relations).values(
             nomenclature_id=nomenclature_id,
             group_id=group_id
         )
@@ -300,30 +341,29 @@ async def add_nomenclature_to_group(token: str, group_id: int, nomenclature_id: 
 async def get_nomenclatures_by_group(token: str, group_id: int):
     """
     Возвращает список ID всех номенклатур из указанной группы.
+
     :param token: Токен пользователя.
     :param group_id: ID группы.
+
     """
+
     # Проверка токена пользователя
     user = await get_user_by_token(token)
     if not user:
         raise HTTPException(status_code=401, detail="Недействительный токен")
 
-    # Запрос на получение только ID номенклатур
-    query = (
-        select(
-            nomenclature.c.id.label("nomenclature_id")
-        )
-        .select_from(
-            nomenclature
-            .join(
-                nomenclature_groups,
-                nomenclature.c.id == nomenclature_groups.c.nomenclature_id
-            )
-        )
-        .where(nomenclature_groups.c.group_id == group_id)
+    # Проверка существования группы
+    group_exists = await database.fetch_one(
+        select(nomenclature_groups.c.id).where(nomenclature_groups.c.id == group_id)
+    )
+    if not group_exists:
+        raise HTTPException(status_code=404, detail=f"Группа с ID {group_id} не существует")
+
+    # Запрос на получение ID номенклатур, связанных с указанной группой
+    query = select(nomenclature_relations.c.nomenclature_id).where(
+        nomenclature_relations.c.group_id == group_id
     )
 
-    # Выполнение SQL-запроса
     results = await database.fetch_all(query)
 
     if not results:
