4e20366562fe006cba6e705673f5b5b4fe922449
Максим Евтушевский <gfer-6@mail.ru>
2025-03-30 02:56:15 +0600

    Fix endpoints manufacturers

--- backend/api/manufacturers/web/views/GetManufacturerByIdView.py
+++ backend/api/manufacturers/web/views/GetManufacturerByIdView.py
@@ -7,6 +7,6 @@ class GetManufacturerByIdView:
     async def __call__(self, token: str, idx: int):
         """Получение производителя по ID"""
         user = await get_user_by_token(token)
-        manufacturer_db = await get_entity_by_id(manufacturers, idx, user.id)
+        manufacturer_db = await get_entity_by_id(manufacturers, idx, user.cashbox_id)
         manufacturer_db = datetime_to_timestamp(manufacturer_db)
         return manufacturer_db
