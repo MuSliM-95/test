aee00364125e75b1446ecdefbb60f5d36c3c6740
matvey . <sa2508mc@gmail.com>
2025-07-10 09:19:26 +0000

    Update docusaurus.config.js

--- submodule-docurus/docusaurus.config.js
+++ submodule-docurus/docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://dev.tablecrm.com',
+  url: 'https://app.tablecrm.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/docasaurus/',
