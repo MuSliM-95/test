f9a91175fa5e20a5eae116c6e631b39d2296357d
matvey . <sa2508mc@gmail.com>
2025-07-25 08:10:26 +0000

    Update backup.sh

--- backup/src/backup.sh
+++ backup/src/backup.sh
@@ -1,9 +1,23 @@
-#! /bin/bash
+#!/bin/bash
 
 set -eu
 set -o pipefail
 
 source ./env.sh
+
+export TZ="Europe/Moscow"
+
+send_message() {
+    local message="$1"
+
+    curl -s -X POST "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage" \
+        -H "Content-Type: application/json" \
+        -d "{
+            \"chat_id\": \"${CHAT_ID}\",
+            \"text\": \"${message}\",
+            \"parse_mode\": \"HTML\"
+        }" > /dev/null
+}
 
 echo "Creating backup of $POSTGRES_DATABASE database..."
 pg_dump --format=plain \
@@ -14,11 +28,17 @@ pg_dump --format=plain \
         $PGDUMP_EXTRA_OPTS \
         > db.sql
 
+BACKUP_KEEP_DAYS=${BACKUP_KEEP_DAYS:-7}
 timestamp=$(date +"%Y-%m-%dT%H:%M:%S")
-s3_uri_base="s3://${S3_BUCKET}/${S3_PREFIX}/${POSTGRES_DATABASE}_${timestamp}.sql"
+today=$(echo "$timestamp" | cut -d'T' -f1)
+cutoff=$(date -d "$today - $BACKUP_KEEP_DAYS days" +"%Y-%m-%d")
+
+backupName="${POSTGRES_DATABASE}_${timestamp}.sql"
+s3_uri_base="s3://${S3_BUCKET}/${S3_PREFIX}/${backupName}"
 
 if [ -n "$PASSPHRASE" ]; then
   echo "Encrypting backup..."
+  rm -f db.sql.gpg
   gpg --symmetric --batch --passphrase "$PASSPHRASE" db.sql
   rm db.sql
   local_file="db.sql.gpg"
@@ -32,18 +52,45 @@ echo "Uploading backup to $S3_BUCKET..."
 aws $aws_args s3 cp "$local_file" "$s3_uri"
 rm "$local_file"
 
-echo "Backup complete."
+send_message "<b>🛡️ Backup database #${POSTGRES_DATABASE} in project #${PROJECT_NAME} successfully</b>
 
-sec=$((86400*${BACKUP_KEEP_DAYS:-14}))
-date_from_remove=$(date -d "@$(($(date +%s) - sec))" +%Y-%m-%d)
-previous_day=$(date -d "yesterday" +"%Y-%m-%d")
-backups_query="Contents[?((LastModified<'${date_from_remove}T00:00:00Z') || (LastModified>='${previous_day}T00:00:00Z' && LastModified<'${previous_day}T22:01:00Z'))].{Key: Key}"
+🏷 File name: <pre><code class='plaintext'>${backupName}</code></pre>
+📁 Full path: <pre><code class='plaintext'>${s3_uri_base}</code></pre>
+🔄 For restore: <pre><code class='shell'>restore.sh ${timestamp}</code></pre>"
 
-echo "Removing old backups from $S3_BUCKET..."
-aws $aws_args s3api list-objects \
-  --bucket "${S3_BUCKET}" \
-  --prefix "${S3_PREFIX}" \
-  --query "${backups_query}" \
-  --output text \
-  | xargs --no-run-if-empty -t -I 'KEY' aws $aws_args s3 rm s3://"${S3_BUCKET}"/'KEY'
-echo "Removal complete."
+if [ -n "$BACKUP_KEEP_DAYS" ]; then
+  echo "Cleaning up old backups..."
+  # Получаем список всех бэкапов
+  backups_json=$(aws $aws_args s3api list-objects \
+    --bucket "${S3_BUCKET}" \
+    --prefix "${S3_PREFIX}" \
+    --query "Contents[?ends_with(Key, '.sql') || ends_with(Key, '.sql.gpg')].{Key: Key, LastModified: LastModified}" \
+    --output json)
+
+  mapfile -t keep_keys < <(echo "$backups_json" | jq -r '.[] | "\(.Key) \(.LastModified)"' | \
+    awk '{split($2, a, "T"); print a[1], $1, $2}' | \
+    sort -k1,1 -k3,3 | \
+    awk '{day=$1; key=$2; time=$3; if (time > times[day] || !(day in times)) { times[day]=time; keys[day]=key } } END { for (d in keys) print keys[d] }')
+
+  echo "$backups_json" | jq -r '.[] | "\(.Key) \(.LastModified)"' | while read -r key last_modified; do
+    backup_date=$(date -d "$last_modified" +"%Y-%m-%d")
+    keep=false
+    for keep_key in "${keep_keys[@]}"; do
+      if [[ "$key" == "$keep_key" ]]; then
+        keep=true
+        break
+      fi
+    done
+    if [[ "$backup_date" < "$cutoff" ]]; then
+      echo "Deleting backup (older than $BACKUP_KEEP_DAYS days): $key"
+      aws $aws_args s3 rm "s3://${S3_BUCKET}/${key}"
+    elif [ "$keep" = false ] && [ "$backup_date" != "$today" ]; then
+      echo "Deleting old backup: $key"
+      aws $aws_args s3 rm "s3://${S3_BUCKET}/${key}"
+    else
+      echo "Keeping backup: $key (Date: $backup_date)"
+    fi
+  done
+
+  echo "Old backups cleanup complete."
+fi
