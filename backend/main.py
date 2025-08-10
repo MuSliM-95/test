71aa208196f6731e5631a3f271a9ae7369b7f468
newspec <malov-2005@bk.ru>
2025-08-10 21:59:45 +0400

    Fix

--- backend/main.py
+++ backend/main.py
@@ -169,6 +169,7 @@ from api.settings.cashbox.routers import router as cashbox_settings_router
 
 
 app = FastAPI(
+    root_path="/api/v1",
     title="TABLECRM API",
     description="Документация API TABLECRM",
     version="1.0"
