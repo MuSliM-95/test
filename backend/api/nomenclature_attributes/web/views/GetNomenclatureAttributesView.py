f9d9969df762c72d65636d7c83534e9475329bec
Daniil Sochnev <sochnevde09@gmail.com>
2025-08-12 11:50:10 +0500

    fix

--- backend/api/nomenclature_attributes/web/views/GetNomenclatureAttributesView.py
+++ backend/api/nomenclature_attributes/web/views/GetNomenclatureAttributesView.py
@@ -11,7 +11,7 @@ class GetNomenclatureAttributesView:
     async def __call__(self, token: str, nomenclature_id: int):
         user = await get_user_by_token(token)
 
-        check_nomenclature_query = select(nomenclature).where(nomenclature.c.id == nomenclature_id, nomenclature.c.owner == user.id)
+        check_nomenclature_query = select(nomenclature).where(nomenclature.c.id == nomenclature_id, nomenclature.c.cashbox == user.cashbox_id)
         res = await database.fetch_one(check_nomenclature_query)
         if not res:
             raise HTTPException(status_code=404,
