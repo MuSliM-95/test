386a19d750eba3d4b8b794229dd7d200610c2ee7
bambutcha <stringdeity@gmail.com>
2025-08-27 10:32:45 +0300

    Add consolidated employee shifts migration

---
+++ backend/database/alembic/versions/687e9c27abd8_add_employee_shifts_system.py
@@ -0,0 +1,24 @@
+"""add_employee_shifts_system
+
+Revision ID: 687e9c27abd8
+Revises: 61daf2cd92aa
+Create Date: 2025-08-27 07:22:19.099254
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '687e9c27abd8'
+down_revision = '61daf2cd92aa'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
