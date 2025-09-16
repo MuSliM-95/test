f750e45ac6d0fe6150ffd249b9352a358e30ec8e
Daniels <daniels.developer48@gmail.com>
2025-09-17 00:58:20 +0300

    get doc_sales by createt date; by delivery date, by id

--- backend/api/docs_sales/sales/infrastructure/repositories/DocsSalesListRepository.py
+++ backend/api/docs_sales/sales/infrastructure/repositories/DocsSalesListRepository.py
@@ -5,6 +5,12 @@ from database.db import contragents, docs_sales, docs_sales_delivery_info
 
 class DocsSalesListRepository:
     @staticmethod
+    def get_doc_sale_by_id(id_doc: int):
+        return docs_sales.select().where(
+            docs_sales.c.id == id_doc, docs_sales.c.is_deleted.is_not(True)
+        )
+
+    @staticmethod
     def base_list_query(user_cashbox_id: int):
         base_query = (
             select(*docs_sales.columns, contragents.c.name.label("contragent_name"))
@@ -30,10 +36,26 @@ class DocsSalesListRepository:
         )
         return count_query
 
+    def get_count_list_by_created_date_query(self, user_cashbox_id: int, start_of_day: int, end_of_day: int):
+        return self.base_count_list_query(user_cashbox_id).where(
+            docs_sales.c.dated >= start_of_day,
+            docs_sales.c.dated <= end_of_day
+        )
+
+    def get_count_list_by_delivery_date_query(self, user_cashbox_id: int, start, end):
+        return self.base_count_list_query(user_cashbox_id).outerjoin(
+        docs_sales_delivery_info,
+        docs_sales_delivery_info.c.docs_sales_id == docs_sales.c.id
+        ).where(
+            docs_sales_delivery_info.c.delivery_date >= start,
+            docs_sales_delivery_info.c.delivery_date <= end
+        )
+
 
     def query_by_created_date(self, user_cashbox_id: int, start: int, end: int):
-        return self.base_list_query(user_cashbox_id) \
-               .where(docs_sales.c.dated >= start, docs_sales.c.dated <= end)
+        return self.base_list_query(user_cashbox_id).where(
+            docs_sales.c.dated >= start, docs_sales.c.dated <= end
+        )
 
 
     def query_by_delivery_date(self, user_cashbox_id: int, start, end):
