23ee41e0d0f832a15095df6a4bd8b0c41a8d9a74
Alexandr Fedorovich <alex2717@yandex.kz>
2025-09-25 21:39:57 +0500

    Update 722d96cec7fb_add_feeds_and_feed_tags.py

--- backend/database/alembic/versions/722d96cec7fb_add_feeds_and_feed_tags.py
+++ backend/database/alembic/versions/722d96cec7fb_add_feeds_and_feed_tags.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '722d96cec7fb'
-down_revision = '3df9a9052a94'
+down_revision = 'c9625a6c9c31'
 branch_labels = None
 depends_on = None
 
