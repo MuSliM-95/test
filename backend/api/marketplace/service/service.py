03b9bbc91d433acea7a0c22b71bc127ac035e7ba
Rustam <rustamvurdihanov@gmail.com>
2025-12-07 14:30:23 +0700

    feature: new endpoint sellers/statics

--- backend/api/marketplace/service/service.py
+++ backend/api/marketplace/service/service.py
@@ -8,11 +8,12 @@ from api.marketplace.service.review_service.service import MarketplaceReviewServ
 from api.marketplace.service.view_event_service.service import MarketplaceViewEventService
 from api.marketplace.service.public_categories.public_categories_service import MarketplacePublicCategoriesService
 from api.marketplace.service.seller_service.service import MarketplaceSellerService
+from api.marketplace.service.seller_statistics_service import MarketplaceSellerStatisticsService
 from common.amqp_messaging.common.core.IRabbitFactory import IRabbitFactory
 from common.utils.ioc.ioc import ioc
 
 
-class MarketplaceService(MarketplaceProductsListService, MarketplaceOrdersService, MarketplaceReviewService, MarketplaceFavoritesService, MarketplaceQrService, MarketplaceViewEventService, MarketplaceLocationsService, MarketplaceCartService, MarketplacePublicCategoriesService, MarketplaceSellerService):
+class MarketplaceService(MarketplaceProductsListService, MarketplaceOrdersService, MarketplaceReviewService, MarketplaceFavoritesService, MarketplaceQrService, MarketplaceViewEventService, MarketplaceLocationsService, MarketplaceCartService, MarketplacePublicCategoriesService, MarketplaceSellerService, MarketplaceSellerStatisticsService):
     def __init__(self):
         super().__init__()
 
